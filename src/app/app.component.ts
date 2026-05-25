d3e1107f10917de06ca60259030811fa88eb16cf
wsc-apk <mathscetpw@gmail.com>
2026-05-25 22:33:04 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
